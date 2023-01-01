eab009a9cb41cb8386c1842aa949d75f8c093514
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2023-01-02 00:53:44 +0100

    Add simple calculator functionality

--- script.js
+++ script.js
@@ -1,68 +1,113 @@
+let displayValue = "";
+let currentOperator = "";
+let firstNum = "";
+let secondNum = "";
+let shouldResetScreen = false;
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operatorButtons = document.querySelectorAll("[data-operator]");
 const currentOperation = document.querySelector(".screen-current");
 const previousOperation = document.querySelector(".screen-before");
 const equalButton = document.querySelector("[data-equal]");
 
-let displayValue = "";
-let currentOperator = "";
-
 numberButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const number = e.target.textContent;
-    displayValue = currentOperation.textContent += number;
-    console.log(displayValue);
+    appendNumber(number);
   });
 });
 
 operatorButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    const operator = e.target.textContent;
-    currentOperator = operator;
-    console.log(currentOperator);
-    updateValueBefore(displayValue, currentOperator);
+    if (currentOperator !== "" && !shouldResetScreen) {
+      operate();
+      updateValueBefore(currentOperator);
+    } else {
+      firstNum = parseFloat(currentOperation.textContent);
+      updateValueBefore(currentOperator);
+    }
+
+    const operator = e.target;
+    currentOperator = operator.textContent;
+    shouldResetScreen = true;
   });
 });
 
 equalButton.addEventListener("click", () => {
-  operate();
-  updateValueBefore();
+  secondNum = parseInt(currentOperation.textContent);
+  operate(currentOperator, firstNum, secondNum);
+  updateValueBefore(currentOperator);
+  equalClear();
 });
 
 // FUNCTIONS
 
-function operate(operator, firstNumber, secondNumber) {
-  switch (operator) {
-    case "%":
-      divide(firstNumber, secondNumber);
-      break;
-    case "-":
-      subtract(firstNumber, secondNumber);
-    case "*":
-      multiply(firstNumber, secondNumber);
-    case "+":
-      add(firstNumber, secondNumber);
-    default:
-      return;
+function equalClear() {
+  currentOperator = "";
+  firstNum = "";
+  secondNum = "";
+  shouldResetScreen = true;
+}
+
+function operate() {
+  if (!shouldResetScreen) {
+    shouldResetScreen = true;
+    secondNum = parseFloat(currentOperation.textContent);
+    calculate(currentOperator, firstNum, secondNum);
   }
+
+  firstNum = parseFloat(currentOperation.textContent);
 }
 
 function add(firstNum, secondNum) {
-  currentOperation.innerHTML = firstNum + secondNum;
+  return firstNum + secondNum;
 }
 
 function subtract(firstNum, secondNum) {
-  currentOperation.innerHTML = firstNum + secondNum;
+  return firstNum - secondNum;
 }
 
 function multiply(firstNum, secondNum) {
-  currentOperation.innerHTML = firstNum + secondNum;
+  return firstNum * secondNum;
 }
 
 function divide(firstNum, secondNum) {
-  currentOperation.innerHTML = firstNum + secondNum;
+  return firstNum / secondNum;
 }
 
-function updateValueBefore(displayValue, currentOperator) {
-  previousOperation.textContent = `${displayValue} ${currentOperator}`;
+// TODO: update before value
+
+// function updateValueBefore(currentOperation) {
+//   if (currentOperation) {
+//     previousOperation.textContent = `${firstNum} ${currentOperation}`;
+//   } else {
+//     previousOperation.textContent = `${firstNum} ${currentOperation} ${secondNum} =`;
+//   }
+// }
+
+function appendNumber(number) {
+  if (displayValue === "0" || shouldResetScreen) {
+    currentOperation.textContent = "";
+    shouldResetScreen = false;
+  }
+  displayValue = currentOperation.textContent += number;
+}
+
+function calculate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "%":
+      currentOperation.innerHTML = divide(firstNumber, secondNumber);
+      break;
+    case "-":
+      currentOperation.innerHTML = subtract(firstNumber, secondNumber);
+      break;
+    case "×":
+      currentOperation.innerHTML = multiply(firstNumber, secondNumber);
+      break;
+    case "+":
+      currentOperation.innerHTML = add(firstNumber, secondNumber);
+      break;
+    default:
+      return;
+  }
 }
